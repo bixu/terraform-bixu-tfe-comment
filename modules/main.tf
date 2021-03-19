570cfad6d68a038deadb4b2fa8c79e96e60f3a37
Blake Irvin <bixu@users.noreply.github.com>
2021-03-19 18:00:36 +0100

    simplify and clarify POST shellcode

--- modules/main.tf
+++ modules/main.tf
@@ -1,7 +1,3 @@
-locals {
-  comment_body = jsonencode({ "body" = var.comment })
-}
-
 resource "null_resource" "post_comment" {
   triggers = {
     timestamp = timestamp()
@@ -13,7 +9,16 @@ curl --silent "https://app.terraform.io/api/v2/runs/$TFC_RUN_ID/comments" \
 --request "POST" \
 --header  "Authorization: Bearer ${var.team_token}" \
 --header  "Content-Type: application/vnd.api+json" \
---data-binary '{"data": {"attributes": "${local.comment_body}" },"type": "comments"} }'
+--data-binary '
+{
+  "data": {
+    "attributes": {
+      "body": "${var.comment}"
+    }
+  },
+  "type": "comments"
+}
+'
 EOT
   }
 }
