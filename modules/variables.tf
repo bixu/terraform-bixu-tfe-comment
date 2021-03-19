a9247c63bd047f011123c29f683ea645bfb06efb
Blake Irvin <bixu@users.noreply.github.com>
2021-03-19 18:43:35 +0100

    format

--- modules/variables.tf
+++ modules/variables.tf
@@ -1,10 +1,10 @@
 variable "comment" {
-  type = string
+  type        = string
   description = "Raw text to be posted as a comment in Terraform Cloud's UI during an `apply` operation."
 }
 
 variable "team_token" {
-  type = string
+  type        = string
   description = "Terraform Cloud token generated at https://app.terraform.io/app/bixu/settings/teams."
   # sensitive = true
 }
